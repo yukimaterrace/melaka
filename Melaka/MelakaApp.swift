d9443d43768832176f5c892d0f2c05e9a0c18fbb
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2023-05-17 17:51:22 +0900

    Add app

---
+++ Melaka/MelakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelakaApp.swift
+//  Melaka
+//
+//  Created by 松尾 勇気（Yuki Matsuo） on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct MelakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
