1ea375812a461fe40ad90f293e663d71840ace2e
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2023-05-25 17:41:11 +0900

    Add DI container

--- Melaka/Source/MelakaApp.swift
+++ Melaka/Source/MelakaApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct MelakaApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
