9004e9ecfe37f42ee87f340fed8f91b2366671a7
Yuki Matsuo <yukimaterrace@gmail.com>
2023-05-28 14:57:52 +0900

    Fix: domain

---
+++ Domain/Source/Service/EnvironmentServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  EnvironmentServiceProtocol.swift
+//  Domain
+//
+//  Created by Yuki Matsuo on 2023/05/28.
+//
+
+import Foundation
+
+public protocol EnvironmentServiceProtocol {
+    var apiBaseUrl: URL { get }
+}
