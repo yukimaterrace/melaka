320ba00be4f4054d4096a2de355b52bad4c8f787
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2023-05-30 16:25:57 +0900

    Add alert

---
+++ Melaka/Source/Screen/Alert/AlertViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  AlertViewModel.swift
+//  Melaka
+//
+//  Created by 松尾 勇気（Yuki Matsuo） on 2023/05/29.
+//
+
+import API
+import Combine
+import Foundation
+
+class AlertViewModel: ObservableObject {
+    private var cancellables = Set<AnyCancellable>()
+    private let errorSubject = PassthroughSubject<Error, Never>()
+
+    @Published var isError: Bool = false
+    @Published var alertTitle: String = ""
+    @Published var alertMessage: String = ""
+
+    init() {
+        bind()
+    }
+
+    func bind() {
+        errorSubject.map { _ in true }.assign(to: \.isError, on: self).store(in: &cancellables)
+        let networkError = errorSubject.compactMap {
+            if let networkError = $0 as? NetworkError {
+                return networkError
+            }
+            return nil
+        }
+
+        let error = errorSubject.filter { !($0 is NetworkError) }
+
+        networkError.map { _ in R.string.localizable.alertNetworkTitle() }
+            .assign(to: \.alertTitle, on: self)
+            .store(in: &cancellables)
+
+        networkError.map { "[\($0.statusCode)]\($0.message ?? "")" }
+            .assign(to: \.alertMessage, on: self)
+            .store(in: &cancellables)
+
+        error.map { _ in R.string.localizable.alertGeneralTitle() }
+            .assign(to: \.alertTitle, on: self)
+            .store(in: &cancellables)
+
+        error.map { $0.localizedDescription }
+            .assign(to: \.alertMessage, on: self)
+            .store(in: &cancellables)
+    }
+
+    func send(error: Error) {
+        errorSubject.send(error)
+    }
+}
---
+++ Melaka/Source/Screen/Alert/View+Alert.swift
@@ -0,0 +1,27 @@
+//
+//  View+Alert.swift
+//  Melaka
+//
+//  Created by 松尾 勇気（Yuki Matsuo） on 2023/05/30.
+//
+
+import SwiftUI
+
+extension View {
+
+    func appAlert(viewModel: Binding<AlertViewModel>) -> some View {
+        alert(
+            viewModel.wrappedValue.alertTitle,
+            isPresented: viewModel.isError,
+            actions: {
+                Button(R.string.localizable.alertButtonClose()) {
+                    viewModel.wrappedValue.isError = false
+                }
+            },
+            message: {
+                Text(viewModel.wrappedValue.alertMessage)
+                    .font(.hiragino(.w3, size: 14))
+            }
+        )
+    }
+}
