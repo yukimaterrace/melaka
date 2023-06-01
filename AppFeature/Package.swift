abac318694ddf8a43e2272ad14d6535d389c802e
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2023-06-01 18:29:34 +0900

    Fix: workflow

--- AppFeature/Package.swift
+++ AppFeature/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,7 +11,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "App", targets: ["App"]),
+        .library(name: "App", targets: ["App"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -33,20 +33,20 @@ let package = Package(
                     .copy("Resource/Font/Font Awesome 6 Free-Solid-900.otf"),
                     .copy("Resource/Environment.plist")
                 ]),
-        
+
         .testTarget(name: "AppTests",
                     dependencies: [.target(name: "App")]),
-        
+
         .target(name: "Domain", dependencies: []),
-        
+
         .testTarget(name: "DomainTests",
                     dependencies: [.target(name: "Domain")]),
-        
+
         .target(name: "API",
                 dependencies: [
                     .product(name: "Alamofire", package: "Alamofire")
                 ]),
-        
+
         .testTarget(name: "APITests",
                     dependencies: [.target(name: "API")])
     ]
